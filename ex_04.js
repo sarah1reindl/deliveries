2a963301eb3cd127b16d72781ed9175f1582cf01
sarah1reindl <sarah1.reindl@epitech.digital>
2024-09-24 17:52:55 +0200

    ex_04.js

---
+++ ex_04.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>JavaScript Pool</title>
+  </head>
+  <body>
+    <h1 id="welcome"></h1>
+
+    <script>
+      // Display on the result page
+      document.getElementById("welcome").innerHTML = "Welcome to the javascript pool! Have some fun and don't forget to bring a towel.";
+
+      // Log to the console
+      console.log("Website launched");
+    </script>
+  </body>
+</html>
